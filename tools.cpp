10d6842e2173dc27cf436ca97b6975834c68cf87
Michel Sénégas <michel.senegas@eleves.enpc.fr>
2021-10-21 16:19:07 +0200

    enlever FMatrix

--- tools.cpp
+++ tools.cpp
@@ -1,6 +1,6 @@
 #include "tools.h"
 
-double p_S(const std::vector<bool>& S,const std::vector<int>& X1,const std::vector<int>& X2,const Imagine::FMatrix<double,N,N>& probability_matrix) {
+double p_S(const std::vector<bool>& S,const std::vector<int>& X1,const std::vector<int>& X2,const Imagine::Matrix<double>& probability_matrix) {
     // S: For all i, whether X1[i] wins agains X2[i]
     // This function computes P(S) given X1 and X2, under the assumption of independence
     assert(S.size()==X1.size());
@@ -13,12 +13,12 @@ double p_S(const std::vector<bool>& S,const std::vector<int>& X1,const std::vect
     }
 }
 
-void opponent_choice_algorithm(std::vector<int>& X1,std::vector<int>& X2,std::deque<double>& ranking,const Imagine::FMatrix<double,N,N>& probability_matrix) {
+void opponent_choice_algorithm(std::vector<int>& X1,std::vector<int>& X2,std::deque<double>& ranking,const Imagine::Matrix<double>& probability_matrix) {
     // X1,X2: For all i, X1[i] will play against X2[i].
     // X1.size(): Number of pairs already decided
     // probability_matrix: Matrix containing all win probabilities between players
     // ranking: players who haven't chosen yet. Read ranking[j]: Player ranked j+1 among those who haven't chosen (after execution, only the 2 last players stay in ranking)
-    if (X1.size()==N/2-1) {
+    if (X1.size()==probability_matrix.nrow()-1) {
         X1.push_back(ranking.front());
         X2.push_back(ranking.back());
     }
@@ -27,6 +27,8 @@ void opponent_choice_algorithm(std::vector<int>& X1,std::vector<int>& X2,std::de
         ranking.pop_front();
     }
 }
+
+
 
 void setDiagonalVictory(Imagine::Matrix<float> &M){
     for(int i=0; i<M.nrow(); i++){
