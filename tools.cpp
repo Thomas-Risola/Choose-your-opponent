b78c854c43592bf9ac8e421a90339637b088af86
Michel Sénégas <michel.senegas@eleves.enpc.fr>
2021-10-21 16:44:53 +0200

    forward_list

--- tools.cpp
+++ tools.cpp
@@ -16,18 +16,23 @@ double p_S(const std::vector<bool>& S,const std::vector<int>& X1,const std::vect
     return rep;
 }
 
-void opponent_choice_algorithm(std::vector<int>& X1,std::vector<int>& X2,std::deque<double>& ranking,const Imagine::Matrix<double>& probability_matrix) {
+void opponent_choice_algorithm(std::vector<int>& X1,std::vector<int>& X2,std::forward_list<double>& ranking,const Imagine::Matrix<double>& probability_matrix) {
     // X1,X2: For all i, X1[i] will play against X2[i].
     // X1.size(): Number of pairs already decided
     // probability_matrix: Matrix containing all win probabilities between players
     // ranking: players who haven't chosen yet. Read ranking[j]: Player ranked j+1 among those who haven't chosen (after execution, only the 2 last players stay in ranking)
     if (X1.size()==probability_matrix.nrow()-1) {
-        X1.push_back(ranking.front());
-        X2.push_back(ranking.back());
+        std::forward_list<double>::iterator pos=ranking.begin();
+        X1.push_back(*pos);
+        pos++;
+        X2.push_back(*pos);
     }
     else {
         int current_player=ranking.front();
         ranking.pop_front();
+        int q=0;
+        for (std::forward_list<double>::iterator pos=ranking.begin();pos!=ranking.end();++pos) {
+        }
     }
 }
 
--- tools.h
+++ tools.h
@@ -3,14 +3,14 @@
 #include "Imagine/LinAlg.h"
 #include <Imagine/Graphics.h>
 #include <vector>
-#include <deque>
+#include <forward_list>
 
 const int N=16;
 
 
 double p_S(const std::vector<bool>& S,const std::vector<int>& X1,const std::vector<int>& X2,const Imagine::Matrix<double>& probability_matrix);
 
-void opponent_choice_algorithm(std::vector<int>& X1,std::vector<int>& X2,std::deque<double>& ranking,const Imagine::Matrix<double>& probability_matrix);
+void opponent_choice_algorithm(std::vector<int>& X1,std::vector<int>& X2,std::forward_list<double>& ranking,const Imagine::Matrix<double>& probability_matrix);
 
 void setMatrixVictory(Imagine::Matrix<double> &M);
 
