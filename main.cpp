f65ac590395f31b821edd789cae58dc1c9f31393
Thomas Risola <thomas.risola@gmail.com>
2021-10-21 15:30:19 +0200

    creation matrice de victoire

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include <Imagine/Graphics.h>
+#include "tools.h"
+
 
 using namespace std;
+
+const int N = 16;
+Imagine::Matrix<float> victoryMatrix(N,N);
 
 int main()
 {
 
-    cout << "Hello World!" << endl;
+    setMatrixVictory(victoryMatrix);
+    display(victoryMatrix);
     return 0;
 }
--- tools.h
+++ tools.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include "Imagine/LinAlg.h"
+#include <Imagine/Graphics.h>
+
+
+
+
+void setMatrixVictory(Imagine::Matrix<float> &M);
+
+void display(const Imagine::Matrix<float> &M);
