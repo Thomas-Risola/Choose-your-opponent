9426b7677190a9c30e8798ce8dd3ed1867b50698
Thomas Risola <thomas.risola@gmail.com>
2021-11-11 17:41:35 +0100

    Creation de la matrice via le site clubelo

---
+++ ParseurMatrix.py
@@ -0,0 +1,80 @@
+# import libraries
+from bs4 import BeautifulSoup
+import urllib3
+import numpy as np
+
+
+class Team:
+    def __init__(self, name, elo):
+        self.name = name
+        self.elo = elo
+
+    def __str__(self):
+        print(self.name + " " + self.elo)
+
+    def __set_name__(self, name):
+        self.name = name
+
+    def __set_elo__(self, elo):
+        self.elo = elo
+
+
+# functions to generate url
+def convert_date_to_string(day, month, year):
+    return str(year) + "-" + str(month) + "-" + str(day)
+
+
+def get_clubelo_url(str_date):
+    return "http://clubelo.com/" + str_date + "/Ranking"
+
+
+# set elos for all teams
+def set_elo_from_soup(soup, team_list):
+    ranking = soup.find("table", attrs={"class": "ranking"})
+    for team in team_list:
+        set_elo_from_ranking(ranking, team)
+
+
+# set elo for one team
+def set_elo_from_ranking(ranking, team):
+    team_info = ranking.find("a", string=team.name).parent.parent
+    # elo is what contain the elo information
+    elo = team_info.find("td", attrs={"class": "r"})
+    # .string to get  only the elo
+    # then we convert into int
+    team.__set_elo__(int(elo.string))
+
+
+# do what you think it does with elo formulas
+def victory_probability_1vs2(team1, team2):
+    diff_elo = team1.elo - team2.elo
+    return 1 / (10 ** (-diff_elo / 400) + 1)
+
+
+# set probabilities into a matrix
+def fill_victory_matrix(team_list):
+    number_of_teams = len(team_list)
+    matrix = np.zeros((number_of_teams, number_of_teams))
+    for i in range(number_of_teams):
+        for j in range(i+1, number_of_teams):
+            matrix[i][j] = victory_probability_1vs2(team_list[i], team_list[j])
+            matrix[j][i] = 1 - matrix[i][j]
+    return matrix
+
+
+def victory_matrix(team_list, day, month, year):
+    # specify the url from the date you want
+    http = urllib3.PoolManager()
+
+    str_date = convert_date_to_string(day, month, year)
+    url = get_clubelo_url(str_date)
+
+    # get page
+    response = http.request('GET', url)
+
+    # make it usable
+    soup = BeautifulSoup(response.data, "html.parser")
+
+    set_elo_from_soup(soup, team_list)
+
+    return fill_victory_matrix(team_list)
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-a = 2
-b = 1
-c = 3
-d = 4
-print(a)
+import ParseurMatrix as pm
+
+if __name__ == '__main__':
+    day = 11
+    month = 12
+    year = 2021
+    team_list = [pm.Team("Man City", 0), pm.Team("Bayern", 0), pm.Team("Liverpool", 0), pm.Team("Chelsea", 0),
+                 pm.Team("Juventus", 0), pm.Team("Paris SG", 0), pm.Team("Dortmund", 0), pm.Team("Real Madrid", 0),
+                 pm.Team("Porto", 0), pm.Team("RB Leipzig", 0), pm.Team("Atlético", 0), pm.Team("Barcelona", 0),
+                 pm.Team("Sevilla", 0), pm.Team("Lazio", 0), pm.Team("Atalanta", 0), pm.Team("Gladbach", 0)]
+
+    victory_matrix = pm.victory_matrix(team_list, day, month, year)
+    print(victory_matrix)
+    # we need to save the matrix to use it in C++ MOPSI Program
+    # or we can try to execute this in the C++ program?
